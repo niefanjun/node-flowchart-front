7a1b90f8cb4e69d460b3dc3d8a8bea20948a6ebb
niefanjun <niefanjun@gmail.com>
2019-07-22 09:28:16 +0800

    修改路由模式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import FlowPage from './Flow';
 import MindPage from './Mind';
 import KoniPage from './Koni';
